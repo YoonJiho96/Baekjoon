b6681335faf136c25f7de3a35789d2da54616188
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-01-30 15:47:36 +0900

    [Silver III] Title: N과 M (1), Time: 264 ms, Memory: 23328 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,45 +4,42 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static boolean[] checked;
-	static int[] lst;
-	static int[] result;
 	static int N, M;
-	static StringBuilder sb;
-
+	static int[] tgt;		// src 배열 유지할 필요 없이. 그냥 인덱스 값으로 활용.
+	static boolean[] select;
+	static StringBuilder sb;	// StringBuilder 출력 변경
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		sb = new StringBuilder();
-		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		checked = new boolean[N];
-
-		lst = new int[N]; // 원본
-		result = new int[M]; // 순열 결과
-		for (int i = 0; i < N; i++) {
-			lst[i] = i + 1;
-		}
+		select = new boolean[N + 1];
+		tgt = new int[M];
 
 		perm(0);
 		System.out.println(sb);
 	}
 
 	static void perm(int tgtIdx) {
+		// 기저 조건
 		if (tgtIdx == M) {
-			for(int n : result)
+			// Complete Code. 완성시 작성할 코드
+			for (int n : tgt) {
 				sb.append(n).append(" ");
+			}
 			sb.append("\n");
 			return;
 		}
-		for (int i = 0; i < lst.length; i++) {
-			if(checked[i]) continue;
-			result[tgtIdx] = lst[i];
-			checked[i] = true;
+
+		for (int i = 1; i <= N; i++) {
+			if(select[i]) continue;
+			tgt[tgtIdx] = i;
+			select[i] = true;
 			perm(tgtIdx + 1);
-			checked[i] = false;
+			select[i] = false;
 		}
 	}
 }
