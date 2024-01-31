b9a782b3a81478e9caf692ff0126ad8d1306a27f
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-01-31 17:05:35 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 828 ms, Memory: 118884 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,39 +4,31 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;
-	static StringTokenizer st;
+	static int[][] accu; // 현재 index 까지 누적된 합을 저장.
 	static StringBuilder sb = new StringBuilder();
-	static int[][] maps;
-	static int[][] sums;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		maps = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
+		accu = new int[N + 1][N + 1]; // 0 dummy
+		for (int i = 1; i <= N; i++) { // 행 (아래)
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				maps[i][j] = Integer.parseInt(st.nextToken());
+			for (int j = 1; j <= N; j++) { // 열 (옆)
+				accu[i][j] = accu[i - 1][j] + accu[i][j - 1] - accu[i - 1][j - 1] + Integer.parseInt(st.nextToken());
 			}
 		}
 
-		sums = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				sums[i][j] = sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1] + maps[i][j];
-			}
-		}
-        
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-			sb.append(sums[x2][y2] - sums[x1 - 1][y2] - sums[x2][y1 - 1] + sums[x1 - 1][y1 - 1]).append("\n");
+			sb.append(accu[x2][y2] - accu[x1-1][y2] - accu[x2][y1-1] + accu[x1-1][y1-1]).append("\n");
 		}
 		System.out.println(sb);
 	}
