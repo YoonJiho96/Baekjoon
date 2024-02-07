4d0ef93c12abee836a6e15feb574ac39168c67fc
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-02-07 15:56:43 +0900

    [Silver I] Title: 절댓값 힙, Time: 376 ms, Memory: 30240 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static PriorityQueue<AbsNode> pqueue = new PriorityQueue<>((o1, o2) -> (o1.absNum == o2.absNum) ? o1.num - o2.num : o1.absNum - o2.absNum);
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int input = Integer.parseInt(st.nextToken());
+			
+			if(input == 0) {
+				// 추출
+				if(pqueue.isEmpty()) {
+					sb.append(0).append("\n");
+				} else {
+					sb.append(pqueue.poll().num).append("\n");
+				}
+			}else {
+				// 삽입
+				pqueue.add(new AbsNode(Math.abs(input), input));
+			}
+		}
+		System.out.println(sb);
+	}
+}
+class AbsNode {
+	int absNum;
+	int num;
+	AbsNode(int a, int n) {
+		absNum = a;
+		num = n;
+	}
+}
