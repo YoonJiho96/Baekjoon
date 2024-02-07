9921d578dd1c132411c61a44deace0fa3dfc4489
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-02-07 17:55:14 +0900

    [D4] Title: 정사각형 방, Time: 154 ms, Memory: 29,452 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -1,19 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-// visit 배열이 필요한가? => 필요 없음
-// * 항상 나보다 값을 탐색하기 때문.
-// delta for 문 break; 추가.
 public class Solution {
 	static int[][] map;
+	static int[][] memoi;
 	static int T, N, NO, COUNT; // 방번호, 이동횟수
 	static int[] dy = { -1, 1, 0, 0 };
 	static int[] dx = { 0, 0, -1, 1 };
 	static StringBuilder sb = new StringBuilder();
-	static Queue<Node> queue = new ArrayDeque<>();
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,7 +16,8 @@ public class Solution {
 
 		for (int t = 1; t <= T; t++) {
 			N = Integer.parseInt(br.readLine());
-			map = new int[N][N];
+			map = new int[N][N];	
+			memoi = new int[N][N];
 
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
@@ -37,48 +33,33 @@ public class Solution {
 			// 모든 좌표에서 다 시도. 그 각각의 결과 중 최선을 선택
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					// queue 초기화. queue.clear(); 는 현재 필요 X
-					queue.add(new Node(i, j, map[i][j], 1));
-					bfs();
+					int cnt = dfs(i, j);
+					int no = map[i][j];
+
+					// 문제 처리
+					if (cnt > COUNT) {
+						COUNT = cnt;
+						NO = no;
+					} else if (cnt == COUNT) {
+						NO = (no < NO) ? no:NO;
+					}
 				}
 			}
-
 			sb.append("#").append(t).append(" ").append(NO).append(" ").append(COUNT).append("\n");
 		}
 		System.out.println(sb);
 	}
-
-	static void bfs() {
-		while (!queue.isEmpty()) {
-			Node node = queue.poll();
-			
-			// 문제 처리
-			if (node.cnt > COUNT) {
-				COUNT = node.cnt;
-				NO = node.no;
-			} else if (node.cnt == COUNT) {
-				NO = (node.no < NO) ? node.no : NO;
-			}
-
-			// 이후 조건에 맞는 더 갈 수 있는 곳 방문
-			for (int i = 0; i < 4; i++) {
-				int ny = node.y + dy[i];
-				int nx = node.x + dx[i];
-				if (ny < 0 || nx < 0 || ny >= N || nx >= N || map[ny][nx] != map[node.y][node.x] + 1) {
-					continue;
-				}
-				queue.offer(new Node(ny, nx, node.no, node.cnt + 1));
+	static int dfs(int y, int x) {
+		if(memoi[y][x] != 0) {
+			return memoi[y][x];
+		}
+		for(int i=0; i<4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if(ny >= 0 && nx >= 0 && ny < N && nx < N && map[ny][nx] == map[y][x] + 1) {
+				return memoi[y][x] = dfs(ny, nx) + 1;
 			}
 		}
-	}
-	static class Node {
-		int y, x, no, cnt;
-
-		Node(int y, int x, int no, int cnt) {
-			this.y = y;
-			this.x = x;
-			this.no = no;
-			this.cnt = cnt;
-		}
+		return memoi[y][x] = 1;
 	}
 }
