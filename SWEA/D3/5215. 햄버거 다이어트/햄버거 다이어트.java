0b4d0f166822eaa59c7a05e62bfb73220a4990ea
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-02-08 14:31:58 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,492 ms, Memory: 23,164 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -3,11 +3,11 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// subset + binary counting
 public class Solution {
 
 	static int T, N, L, max;
 	static Item[] src;
-	static boolean[] select;
 	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws Exception {
@@ -20,45 +20,29 @@ public class Solution {
 			L = Integer.parseInt(st.nextToken());
 
 			src = new Item[N];
-			select = new boolean[N];
 
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				src[i] = new Item(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 			}
 
-			// 풀이
 			max = 0;
+			int subsetCnt = 1 << N;
 
-			subset(0);
+			for(int i=0; i<subsetCnt; i++) {
+				int cal = 0;
+				int point = 0;
 
+				for(int j=0; j<N; j++) {
+					if((i & 1 << j) == 0 ) continue;
+					cal += src[j].c;
+					point += src[j].p;
+				}
+				if(cal <= L) max = Math.max(max, point);
+			}
 			sb.append("#").append(t).append(" ").append(max).append("\n");
 		}
 		System.out.println(sb);
-	}
-
-	static void subset(int srcIdx) {
-		// 기저조건
-		if (srcIdx == N) {
-			// complete code
-			int cal = 0;
-			int point = 0;
-			
-			for(int i=0; i<N; i++) {
-				if(!select[i]) continue; 
-				cal += src[i].c;
-				point += src[i].p;
-			}
-			// 조건에 맞는 max 갱신
-			if(cal <= L) max = Math.max(max, point);
-
-			return;
-		}
-
-		select[srcIdx] = true;
-		subset(srcIdx + 1);
-		select[srcIdx] = false;
-		subset(srcIdx + 1);
 	}
 
 	static class Item {
