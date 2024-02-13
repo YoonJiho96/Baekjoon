46389fad1cda5b9991df484c338a60ab8ca9ac53
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-02-13 16:44:30 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 909 ms, Memory: 23,588 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -3,81 +3,85 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Solution {
-	static int[] input;
-	static int[] src;
-	static int[] tgt;
-	static boolean[] select;
-	static int win, lose;
+public class Solution{
+	static int T, win, lose, N = 9;
+	static int[] input = new int[19]; // 0 : dummy, 규영이의 카드 입력을 여기에 표시 => 인영이의 카드를 구성
+	static int[] guCard = new int[9]; // 0 : dummy <= 고정
+	static int[] inCard = new int[9]; // 0 : dummy <= 다양한 순열 src
+
 	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
+			// 변수 초기화
+			win = lose = 0;
+			Arrays.fill(input, 0); // 테케 별 초기화.
+
+			// 입력
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			input = new int[9]; // 규영 카드
-			tgt = new int[9]; // 상대 카드 경우의 수
-			src = new int[9]; // 상대 카드
-			select = new boolean[9];
-
-			// 규영 카드 초기화
-			for (int i = 0; i < 9; i++) {
-				input[i] = Integer.parseInt(st.nextToken());
+			// 규영이 카드 처리
+			int num = 0;
+			for (int i = 0; i < N; i++) {
+				num = Integer.parseInt(st.nextToken());
+				input[num] = 1;
+				guCard[i] = num;
 			}
-
-			// 상대 카드 초기화
-			int[] temp = new int[19];
-			for (int a : input) {
-				temp[a] = 1;
-			}
-			int count = 0;
+			// 인영 카드 처리 <= input 배열에 0으로 남은 index.
+			num = 0;
 			for (int i = 1; i <= 18; i++) {
-				if (temp[i] != 1) {
-					src[count++] = i;
-				}
+				if (input[i] == 0)
+					inCard[num++] = i;
 			}
 
-			win = 0;
-			lose = 0;
-			perm(0);
+			// 가장 작은 수로 정렬 <= 경우의 하나 ( 이미 정렬 되어 있다.
+			while(true) {
+				check();
+				if(!np(inCard)) break;
+			}
+			
 			sb.append("#").append(t).append(" ").append(win).append(" ").append(lose).append("\n");
 		}
 		System.out.println(sb);
 	}
-
-	static void perm(int tgtIdx) {
-		if (tgtIdx == 9) {
-			checkWin();
-			return;
+	
+	// NP(), swap()
+	static boolean np(int[] array) {
+		int i=array.length-1;
+		while(i>0 && array[i-1]>=array[i]) i--;
+		
+		// 맨 앞 or 중간
+		if(i==0) return false; // 현재가 가장 큰 수, 더 이상 새로운 더 큰 수를 만들 수 없다.
+		// 더 큰 수를 만들 수 있다.
+		int j=array.length -1;
+		while(array[i-1] >= array[j]) j--;
+		swap(array, i-1, j);
+		
+		// 뒤쪽 정리
+		int k=array.length-1;
+		while(i<k) {
+			swap(array, i++, k--);
 		}
-
-		for (int i = 0; i < 9; i++) {
-			if (select[i])
-				continue;
-
-			tgt[tgtIdx] = src[i];
-			select[i] = true;
-			perm(tgtIdx + 1);
-			select[i] = false;
-		}
+		return true;
 	}
-	static void checkWin() {
-		int aSum = 0;
-		int bSum = 0;
+	
+	static void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
 
-		for (int i = 0; i < 9; i++) {
-			int a = input[i];
-			int b = tgt[i];
-			if (a > b)
-				aSum += a + b;
-			else
-				bSum += a + b;
+	static void check() {
+		int guSum = 0;
+		int inSum = 0;
+		
+		for(int i=0; i<N; i++) {
+			if(guCard[i] > inCard[i] ) guSum += guCard[i] + inCard[i];
+			else inSum += guCard[i] + inCard[i];
 		}
-		if (aSum > bSum)
-			win += 1;
-		else if (aSum < bSum)
-			lose += 1;
+		if(guSum>inSum) win++;
+		else if(guSum<inSum) lose++;
 	}
 }
