2569553d51be3c6c92d496e1aebb119622e963c9
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-02-16 21:45:38 +0900

    [Gold IV] Title: 알파벳, Time: 864 ms, Memory: 15168 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,17 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int R, C, max;
 	static char[][] input;
-	static HashMap<Character, Integer> map;
+	static boolean[] visit = new boolean[26];
 	static int[] dy = { -1, 1, 0, 0 };
 	static int[] dx = { 0, 0, -1, 1 };
-	static List<Character> temp = new ArrayList<>();
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,26 +21,25 @@ public class Main {
 		}
 
 		max = 0;
-		map = new HashMap<>();
+		visit[input[0][0] - 'A'] = true;
 		dfs(0, 0, 1);
 		System.out.println(max);
 	}
 
 	static void dfs(int y, int x, int dis) {
-		if (map.containsKey(input[y][x])) {
-			return;
-		}
-		map.put(input[y][x], 0);
 		max = Math.max(max, dis);
 
 		for (int i = 0; i < 4; i++) {
 			int nY = y + dy[i];
 			int nX = x + dx[i];
-
 			if (nY < 0 || nY >= R || nX < 0 || nX >= C)
 				continue;
+			if (visit[input[nY][nX] - 'A']) {
+				continue;
+			}
+			visit[input[nY][nX] - 'A'] = true;
 			dfs(nY, nX, dis + 1);
+			visit[input[nY][nX] - 'A'] = false;
 		}
-		map.remove(input[y][x]);
 	}
 }
