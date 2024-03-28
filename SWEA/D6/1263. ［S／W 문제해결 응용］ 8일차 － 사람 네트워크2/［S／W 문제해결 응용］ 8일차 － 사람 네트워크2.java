e49086c576246dc8a1ef9b588ca919ea887d2992
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-03-28 12:17:19 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 4,182 ms, Memory: 105,952 KB -BaekjoonHub

---
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int[][] adjMatrix;
+	static int INF = 10000;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+
+			adjMatrix = new int[N][N];
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					adjMatrix[i][j] = Integer.parseInt(st.nextToken());
+					if (adjMatrix[i][j] == 0) {
+						adjMatrix[i][j] = INF;
+					}
+				}
+			}
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					for (int k = 0; k < N; k++) {
+						// i 를 거쳐감
+						int jToi = adjMatrix[j][i];
+						int iTok = adjMatrix[i][k];
+						int jTok = adjMatrix[j][k];
+						if (jToi != 10000 && iTok != 10000) {
+							adjMatrix[j][k] = Math.min(jTok, jToi + iTok);
+						}
+					}
+				}
+			}
+
+			int ans = Integer.MAX_VALUE;
+			for (int i = 0; i < N; i++) {
+				int count = 0;
+				for (int j = 0; j < N; j++) {
+					if (i != j) {
+						count += adjMatrix[i][j];
+					}
+				}
+				ans = Math.min(ans, count);
+			}
+			sb.append("#").append(t).append(" ").append(ans).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
