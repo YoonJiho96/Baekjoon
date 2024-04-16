853f6b5d981e56b7bcb4630d689d1a5e938840da
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-04-16 17:46:59 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1268 ms, Memory: 331952 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;
 	static int[] parent;
-	static List<int[]> list = new ArrayList<>();
+	static PriorityQueue<int[]> pqueue = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,23 +15,22 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		parent = new int[N + 1];
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
-			list.add(new int[] { from, to, v });
+			pqueue.offer(new int[] { from, to, v });
 		}
 
-		Collections.sort(list, (o1, o2) -> o1[2] - o2[2]);
+		parent = new int[N + 1];
 		makeSet();
-		
+
 		int result = 0;
 		int count = 0;
 		for (int i = 0; i < M; i++) {
-			int[] e = list.get(i);
+			int[] e = pqueue.poll();
 
 			if (union(e[0], e[1])) {
 				result += e[2];
@@ -49,33 +46,28 @@ public class Main {
 	}
 
 	static void makeSet() {
-		for (int i = 1; i <= N; i++) {
+		for (int i = 1; i <= N; i++)
 			parent[i] = i;
-		}
 	}
 
 	// a 의 부모(연결) 찾기
 	static int findSet(int a) {
-		if (parent[a] == a) {
+		if (parent[a] == a)
 			return a;
-		} else {
+		else
 			return parent[a] = findSet(parent[a]);
-		}
 	}
 
 	static boolean union(int x, int y) {
 		int px = findSet(x);
 		int py = findSet(y);
 
-		if (px == py) {
+		if (px == py)
 			return false;
-		}
-
-		if (px < py) {
+		if (px < py)
 			parent[py] = px;
-		} else {
+		else
 			parent[px] = py;
-		}
 		return true;
 	}
 }
