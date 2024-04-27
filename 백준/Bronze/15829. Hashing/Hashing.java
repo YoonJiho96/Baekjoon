e104133a8264ae4479923bd6adbfc4b3600ac6af
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-04-28 01:56:42 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14212 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -10,12 +10,19 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		char[] input = br.readLine().toCharArray();
 		long sum = 0;
+		
+		long[] pow31 = new long[50];
+		pow31[0] = 1;
+		for(int i=1; i<50; i++) {
+			pow31[i] = (pow31[i-1] * 31) % M;
+		}
+		
 		for (int i = 0; i < N; i++) {
-			long num = (input[i] - 96) * (long) Math.pow(31, i);
+			long num = (input[i] - 96) * pow31[i];
 			num %= M;
 			sum += num;
 		}
-		long result = sum;
+		long result = sum % M;
 		System.out.println(result);
 	}
 }
