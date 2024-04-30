75fb6c334c3ad884ee90da35142e2c5a0a0ef7df
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2024-05-01 02:40:25 +0900

    [Silver II] Title: 마인크래프트, Time: 756 ms, Memory: 34324 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M, B, timeAns = Integer.MAX_VALUE, heightAns = 0;
-	static List<Integer> input = new ArrayList<>();
+	static int[][] input;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,37 +14,37 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		B = Integer.parseInt(st.nextToken());
 
+		input = new int[N][M];
+
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				input.add(Integer.parseInt(st.nextToken()));
+				input[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
 		// 최소 시간. 최대 높이
 		for (int h = 0; h <= 256; h++) { // h: 목표 높이
-			if (h < heightAns)
-				continue;
-
 			int blocks = B; // 블록 현황
 			int getBlock = 0;
 			int usedBlock = 0; // 사용한 블록
 
-			for (int i = 0; i < input.size(); i++) {
-				int cur = input.get(i);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					int cur = input[i][j];
+					int left = h - cur; // +:쌓아야할거, -:빼야할거
 
-				int left = h - cur; // +:쌓아야할거, -:빼야할거
-				if (left >= 0) {
-					blocks -= left; // 블록 사용
-					usedBlock += left;
-				} else {
-					blocks += Math.abs(left);
-					getBlock += Math.abs(left); // 저장
+					if (left >= 0) {
+						blocks -= left; // 블록 사용
+						usedBlock += left;
+					} else {
+						blocks += -left;
+						getBlock += -left; // 저장
+					}
+
 				}
 			}
-
 			if (blocks >= 0) { // 남은 블록
-				// 획득한 경우
 				int time = getBlock * 2 + usedBlock;
 				if (time <= timeAns) {
 					timeAns = time;
