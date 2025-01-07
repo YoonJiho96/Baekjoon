29f97b017b76f0737c031fb9c8a87e83366e6a4a
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-01-07 21:37:21 +0900

    [Bronze II] Title: 커트라인, Time: 112 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length - K]);
+    }
+}
