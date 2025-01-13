64ed425200fcce6c358046ed7cac22ea441f51fc
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-01-13 13:30:30 +0900

    [Bronze I] Title: 최소공배수, Time: 160 ms, Memory: 14988 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println((a * b) / gcd(a, b));
+        }
+    }
+
+    static int gcd(int a, int b) {
+        int n;
+        while (b != 0) {
+            n = a % b;
+            a = b;
+            b = n;
+        }
+        return a;
+    }
+}
