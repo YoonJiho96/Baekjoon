02725ab207b37140e295cc0f949e0aef4e3b17ca
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-01-17 20:32:03 +0900

    [Silver IV] Title: 다음 소수, Time: 276 ms, Memory: 14824 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            long n = Long.parseLong(br.readLine());
+
+            while (!isPrime(n)) {
+                n++;
+            }
+            System.out.println(n);
+        }
+    }
+
+    static boolean isPrime(long n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
