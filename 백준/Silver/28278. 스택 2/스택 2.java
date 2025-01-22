0921f5f92edfe1e8c5e8109ce34000b33c8ee755
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-01-22 22:24:30 +0900

    [Silver IV] Title: 스택 2, Time: 852 ms, Memory: 239852 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            switch (Integer.parseInt(st.nextToken())) {
+                case 1:
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case 2:
+                    sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+                    break;
+                case 3:
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case 4:
+                    sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case 5:
+                    sb.append(!stack.isEmpty() ? stack.peek() : -1).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
