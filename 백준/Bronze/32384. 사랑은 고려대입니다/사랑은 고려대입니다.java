cf820f74c13c7eb28a57b0d94d37bb30bb597d9a
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-02-13 21:30:27 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while(N-- > 0) {
+            sb.append("LoveisKoreaUniversity ");
+        }
+        System.out.println(sb);
+    }
+}
