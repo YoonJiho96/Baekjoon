edc5c92f0e663a711f4729838b38ee3c4ab3c38b
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-02-14 23:40:48 +0900

    [Silver I] Title: 오르막 수, Time: 172 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[][] dp = new int[N + 1][10];
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k <= j; k++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][k]) % 10007;
+                }
+            }
+        }
+        System.out.println(Arrays.stream(dp[N]).sum() % 10007);
+    }
+}
