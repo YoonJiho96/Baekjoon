bdc29217bcda941257730a9a488649fff5c72d14
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-02-15 00:24:49 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 228 ms, Memory: 21228 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        PrintWriter out = new PrintWriter(System.out);
 
         int[][][] ws = new int[21][21][21];
         for (int i = 0; i < 21; i++) {
@@ -16,24 +17,26 @@ public class Main {
                         ws[i][j][k] = 1;
                     } else if (i < j && j < k) {
                         ws[i][j][k] = ws[i][j][k - 1] + ws[i][j - 1][k - 1] - ws[i][j - 1][k];
-
                     } else {
-                        ws[i][j][k] = ws[i - 1][j][k] + ws[i - 1][j - 1][k] + ws[i - 1][j][k - 1] - ws[i - 1][j - 1][k - 1];
+                        ws[i][j][k] = ws[i - 1][j][k]
+                                + ws[i - 1][j - 1][k]
+                                + ws[i - 1][j][k - 1]
+                                - ws[i - 1][j - 1][k - 1];
                     }
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
         while (true) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-
             if (a == -1 && b == -1 && c == -1) break;
-            sb.append(String.format("w(%d, %d, %d) = ", a, b, c));
 
+            sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ");
             if (a <= 0 || b <= 0 || c <= 0) {
                 sb.append(ws[0][0][0]).append("\n");
             } else if (a > 20 || b > 20 || c > 20) {
@@ -42,6 +45,9 @@ public class Main {
                 sb.append(ws[a][b][c]).append("\n");
             }
         }
-        System.out.println(sb);
+
+        out.print(sb);
+        out.flush();
+        out.close();
     }
 }
