45f23e7d2c8d7a76b2860906689ec8aeb9976d3e
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-02-15 00:08:13 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1320 ms, Memory: 92828 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -3,29 +3,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[][][] dp = new int[21][21][21];
+
+    public static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+        if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        }
+        if (dp[a][b][c] != 0) {
+            return dp[a][b][c];
+        }
+        if (a < b && b < c) {
+            dp[a][b][c] = w(a, b, c - 1)
+                    + w(a, b - 1, c - 1)
+                    - w(a, b - 1, c);
+        } else {
+            dp[a][b][c] = w(a - 1, b, c)
+                    + w(a - 1, b - 1, c)
+                    + w(a - 1, b, c - 1)
+                    - w(a - 1, b - 1, c - 1);
+        }
+        return dp[a][b][c];
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int size = 71;
-        int[][][] dp = new int[size][size][size];
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                for (int k = 0; k < size; k++) {
-                    if (i <= 50 || j <= 50 || k <= 50) {
-                        dp[i][j][k] = 1;
-                    } else if (i < j && j < k) {
-                        dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k];
-                    } else {
-                        dp[i][j][k] = dp[i - 1][j][k]
-                                + dp[i - 1][j - 1][k]
-                                + dp[i - 1][j][k - 1]
-                                - dp[i - 1][j - 1][k - 1];
-                    }
-                }
-            }
-        }
 
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -33,17 +38,11 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            if (a == -1 && b == -1 && c == -1) break;
-
-            int result;
-            if (a <= 0 || b <= 0 || c <= 0) {
-                result = 1;
-            } else if (a > 20 || b > 20 || c > 20) {
-                result = dp[70][70][70];
-            } else {
-                result = dp[a + 50][b + 50][c + 50];
+            if (a == -1 && b == -1 && c == -1) {
+                break;
             }
 
+            int result = w(a, b, c);
             sb.append(String.format("w(%d, %d, %d) = %d\n", a, b, c, result));
         }
         System.out.print(sb);
