92a62cd2d1c5f0e83524828e6ca56aca93c1dccb
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-02-16 23:45:18 +0900

    [Silver II] Title: 상자넣기, Time: 116 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,20 +16,18 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-
-        int max = 1;
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int N : arr) {
+            int pos = Collections.binarySearch(list, N);
+            if (pos < 0) {
+                pos = -(pos + 1);
             }
-            max = Math.max(max, dp[i]);
+            if (pos == list.size()) {
+                list.add(N);
+            } else {
+                list.set(pos, N);
+            }
         }
-
-        System.out.println(max);
+        System.out.println(list.size());
     }
 }
