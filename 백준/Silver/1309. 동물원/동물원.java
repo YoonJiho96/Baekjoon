3a2c7f4bba92da59812ba5bdccde49738fb2832e
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-02-18 21:52:50 +0900

    [Silver I] Title: 동물원, Time: 176 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -7,16 +7,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        // 0 : 없는 경우, 1 : 왼쪽, 2: 오른쪽
-        long[][] dp = new long[N + 1][3];
-        dp[1][0] = dp[1][1] = dp[1][2] = 1;
+        int none = 1;
+        int left = 1;
+        int right = 1;
 
         for (int i = 2; i <= N; i++) {
-            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
-            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+            int curNone = (none + left + right) % MOD;
+            int curLeft = (none + right) % MOD;
+            int curRight = (none + left) % MOD;
+
+            none = curNone;
+            left = curLeft;
+            right = curRight;
         }
 
-        System.out.println((dp[N][0] + dp[N][1] + dp[N][2]) % MOD);
+        System.out.println((none + left + right) % MOD);
     }
 }
