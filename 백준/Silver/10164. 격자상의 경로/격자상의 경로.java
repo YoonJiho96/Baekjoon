7bfbeffea9192472259bfe1da7d4d9ed966232b9
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-02-24 00:15:33 +0900

    [Silver II] Title: 격자상의 경로, Time: 176 ms, Memory: 17924 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -1,32 +1,35 @@
-import java.util.Arrays;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
+    public static BigInteger combination(int n, int k) {
+        BigInteger result = BigInteger.ONE;
+        if (k > n - k) {
+            k = n - k;
+        }
+        for (int i = 1; i <= k; i++) {
+            result = result.multiply(BigInteger.valueOf(n - i + 1));
+            result = result.divide(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
         int K = sc.nextInt();
 
-        int[][] dp = new int[N][M];
-        Arrays.fill(dp[0], 1);
-        for (int i = 0; i < N; i++) {
-            dp[i][0] = 1;
-        }
-        for (int i = 1; i < N; i++) {
-            for (int j = 1; j < M; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
-
         if (K == 0) {
-            System.out.println(dp[N - 1][M - 1]);
+            BigInteger paths = combination((N - 1) + (M - 1), N - 1);
+            System.out.println(paths);
         } else {
-            int y = (K / M) + 1;
-            int x = K - ((y - 1) * M);
-            int y2 = N - y + 1;
-            int x2 = M - x + 1;
-            System.out.println(dp[y - 1][x - 1] * dp[y2 - 1][x2 - 1]);
+            int r = (K - 1) / M + 1;
+            int c = (K - 1) % M + 1;
+
+            BigInteger firstPart = combination(r + c - 2, r - 1);
+            BigInteger secondPart = combination((N - r) + (M - c), N - r);
+            System.out.println(firstPart.multiply(secondPart));
         }
     }
 }
