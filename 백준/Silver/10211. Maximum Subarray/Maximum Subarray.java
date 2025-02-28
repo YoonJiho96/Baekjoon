2c2e3c86a9e40327f9f2933d9fe11bb1373e9be8
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-02-28 21:52:48 +0900

    [Silver IV] Title: Maximum Subarray, Time: 244 ms, Memory: 23764 KB -BaekjoonHub

---
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int[] arr = new int[N + 1];
+            int[] dp = new int[N + 1];  // i 까지의 누적합
+            for (int i = 1; i <= N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+                if (i == 1) {
+                    dp[i] = arr[i];
+                } else {
+                    dp[i] = dp[i - 1] + arr[i];
+                }
+            }
+
+            int max = Integer.MIN_VALUE;
+            for (int i = 1; i <= N; i++) {
+                max = Math.max(max, dp[i]);
+                max = Math.max(max, arr[i]);
+                for (int j = 1; j < i; j++) {
+                    int num = dp[i] - dp[j];
+                    max = Math.max(max, num);
+                }
+            }
+            System.out.println(max);
+        }
+    }
+}
