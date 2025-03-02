a83b48c744ab1266aaa5d4f0ae8dc1003ff09c1f
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-03-02 19:27:35 +0900

    [Silver IV] Title: 점화식, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/13699. 점화식/점화식.java
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
         long[] arr = new long[N + 1];
         arr[0] = 1;
