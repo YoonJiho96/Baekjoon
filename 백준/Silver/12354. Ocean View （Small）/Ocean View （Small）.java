11c25221f895aa26be785863da15179381ac78bc
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-03-03 21:22:15 +0900

    [Silver V] Title: Ocean View (Small), Time: 136 ms, Memory: 15464 KB -BaekjoonHub

---
+++ 백준/Silver/12354. Ocean View （Small）/Ocean View （Small）.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int[] dp = new int[N];
+            Arrays.fill(dp, 1);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (arr[i] > arr[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+            
+            int ans = Arrays.stream(dp).max().getAsInt();
+            sb.append("Case #").append(t).append(": ").append(N - ans).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
