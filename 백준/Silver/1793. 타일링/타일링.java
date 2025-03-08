f752f2976a90e51364665c6fe4412ff432f2b85a
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-03-08 22:41:23 +0900

    [Silver II] Title: 타일링, Time: 128 ms, Memory: 14972 KB -BaekjoonHub

--- 백준/Silver/1793. 타일링/타일링.java
+++ 백준/Silver/1793. 타일링/타일링.java
@@ -1,11 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.Scanner;
 
 public class Main {
     static final int SIZE = 250;
 
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         BigInteger[] dp = new BigInteger[SIZE + 1];
         dp[0] = BigInteger.ONE;
@@ -17,8 +18,9 @@ public class Main {
             dp[i] = dp[i - 1].add(dp[i - 2].multiply(BigInteger.valueOf(2)));
         }
 
-        while (sc.hasNextInt()) {
-            int n = sc.nextInt();
+        String input = null;
+        while ((input = br.readLine()) != null) {
+            int n = Integer.parseInt(input);
             System.out.println(dp[n]);
         }
     }
