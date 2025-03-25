a97107fe657c89b9ab2c2e2a7d715cee8d844ace
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-03-25 23:26:15 +0900

    [Gold V] Title: 퇴사 2, Time: 648 ms, Memory: 304348 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -7,26 +7,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[][] arr = new int[N + 1][2];
+        int[] dp = new int[N + 2];
         for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int T = Integer.parseInt(st.nextToken());
             int P = Integer.parseInt(st.nextToken());
-            arr[i][0] = T;
-            arr[i][1] = P;
-        }
 
-        int[] dp = new int[N + 2];
-        for (int i = 1; i <= N; i++) {
-            int prev = i - 1;
-            int next = i + arr[i][0];
-            int cost = arr[i][1];
-
-            dp[i] = Math.max(dp[i], dp[prev]);
-            if (next <= N + 1) {
-                dp[next] = Math.max(dp[next], dp[i] + cost);
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+            if (i + T <= N + 1) {
+                dp[i + T] = Math.max(dp[i + T], dp[i] + P);
             }
         }
+
         System.out.println(Math.max(dp[N], dp[N + 1]));
     }
 }
