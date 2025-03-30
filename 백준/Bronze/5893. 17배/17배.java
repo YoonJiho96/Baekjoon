366783a38e48c304e6f83341d97707700305780c
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-03-30 21:55:09 +0900

    [Bronze III] Title: 17배, Time: 108 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/5893. 17배/17배.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger num = new BigInteger(br.readLine(), 2);
+        num = num.multiply(BigInteger.valueOf(17));
+        System.out.println(num.toString(2));
+    }
+}
