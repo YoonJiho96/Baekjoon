67be79aa74c4ea5423177c30b0c0373d6a098694
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-04-02 21:15:19 +0900

    [Gold IV] Title: 줄세우기, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -18,13 +18,21 @@ public class Main {
         List<Integer> list = new ArrayList<>();
         for (int n : arr) {
             int pos = Collections.binarySearch(list, n);
+
+            // Collections.binarySearch(List<T> list, T key)
+            // 1. key 가 list 안에 있으면 => key 인덱스 그대로 반환 
+            // 2. key 가 list 안에 없으면 => [-(삽입 위치) - 1] 로 반환
             if (pos < 0) {
                 pos = -pos - 1;
             }
 
+            // LIS 길이 구하기
             if (pos == list.size()) {
+                // 숫자가 LIS의 마지막 값 보다 큼 => LIS 길이 증가
                 list.add(n);
             } else {
+                // 중간 어딘가에 값이 들어갈 수 있음
+                // => 자동으로 더 작은 값으로 교체됨
                 list.set(pos, n);
             }
         }
