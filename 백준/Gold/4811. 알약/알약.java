76beb197c093829e8d64c22c64ee9d83d29fbbcb
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-04-08 00:41:31 +0900

    [Gold V] Title: 알약, Time: 116 ms, Memory: 20024 KB -BaekjoonHub

---
+++ 백준/Gold/4811. 알약/알약.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static long[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            int N = Integer.parseInt(br.readLine());
+            if (N == 0) break;
+
+            // dp[i][j] = 한 개 i, 반 개 j 일 때 경우의 수
+            dp = new long[31][31];
+            sb.append(dfs(N, 0)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static long dfs(int w, int h) {
+        if (w == 0 && h == 0) return 1;
+        if (dp[w][h] != 0) return dp[w][h];
+
+        long ans = 0;
+        if (w > 0) ans += dfs(w - 1, h + 1);
+        if (h > 0) ans += dfs(w, h - 1);
+
+        dp[w][h] = ans;
+        return ans;
+    }
+}
