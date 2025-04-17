2cbff2d56e20672c71f3393c99fa4065156e7d5f
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-04-17 19:52:22 +0900

    [Silver I] Title: 엔비스카의 영혼, Time: 136 ms, Memory: 18132 KB -BaekjoonHub

--- 백준/Silver/16568. 엔비스카의 영혼/엔비스카의 영혼.java
+++ 백준/Silver/16568. 엔비스카의 영혼/엔비스카의 영혼.java
@@ -1,5 +1,7 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -10,26 +12,21 @@ public class Main {
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        if (N == 0) {
-            System.out.println(0);
-            return;
-        }
-
-        // 이동 집합 M = {1, 1+a, 1+b}
-        int[] moves = Arrays.stream(new int[]{1, 1 + a, 1 + b})
-                .distinct().sorted().toArray();
-
-        int INF = 1_000_000_007;
         int[] dp = new int[N + 1];
-        Arrays.fill(dp, INF);
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
 
-        for (int x = 1; x <= N; x++) {
-            for (int k : moves) {
-                if (k > x) break;
-                dp[x] = Math.min(dp[x], dp[x - k] + 1);
+        for (int i = 1; i <= N; i++) {
+            dp[i] = Math.min(dp[i - 1] + 1, dp[i]);
+
+            if (i - a > 0) {
+                dp[i] = Math.min(dp[i - a - 1] + 1, dp[i]);
+            }
+            if (i - b > 0) {
+                dp[i] = Math.min(dp[i - b - 1] + 1, dp[i]);
             }
         }
+
         System.out.println(dp[N]);
     }
 }
