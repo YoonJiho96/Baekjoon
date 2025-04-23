5ae227f66459f0839e2849e07d71741a43f4263e
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-04-23 21:42:10 +0900

    [Silver IV] Title: 파스칼 삼각형, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.java
+++ 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.java
@@ -11,21 +11,21 @@ public class Main {
         int C = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[31][31];
-        arr[1][1] = 1;
-        for (int i = 2; i <= 30; i++) {
+        int MAX = R + W;
+        int[][] arr = new int[MAX][MAX];
+        arr[0][0] = 1;
+        for (int i = 1; i < MAX; i++) {
+            arr[i][0] = 1;
             for (int j = 1; j <= i; j++) {
                 arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
             }
         }
 
         int result = 0;
-        int count = 1;
-        for (int i = R; i < (R + W); i++) {
-            for (int j = 0; j < count; j++) {
-                result += arr[i][C + j];
+        for (int i = 0; i < W; i++) {
+            for (int j = 0; j <= i; j++) {
+                result += arr[R + i - 1][C + j - 1];
             }
-            count++;
         }
         System.out.println(result);
     }
