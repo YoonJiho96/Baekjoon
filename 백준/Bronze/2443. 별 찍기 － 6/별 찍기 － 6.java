98af21fc1c286cade7a2af5117e922642a41cdfa
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-05-15 17:55:10 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            sb.append(" ".repeat(i)).append("*".repeat((N - i) * 2 - 1)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
