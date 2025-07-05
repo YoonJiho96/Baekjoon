9159de1d0dd13434abeb3997d5dddbd9a28a0ed6
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2025-07-06 01:52:07 +0900

    [Bronze III] Title: 디지털 루트, Time: 92 ms, Memory: 13976 KB -BaekjoonHub

--- 백준/Bronze/6378. 디지털 루트/디지털 루트.java
+++ 백준/Bronze/6378. 디지털 루트/디지털 루트.java
@@ -2,24 +2,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static int digitalRoot(String num) {
+        int mod = 0;
+        for (int i = 0; i < num.length(); i++) {
+            mod = (mod + (num.charAt(i) - '0')) % 9;
+        }
+        return mod == 0 ? 9 : mod;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         while (true) {
-            String N = br.readLine().trim();
-            if (N.equals("0")) break;
-
-            while (N.length() > 1) {
-                int sum = 0;
-                for (char c : N.toCharArray()) {
-                    sum += c - '0';
-                }
-                N = String.valueOf(sum);
-            }
-
-            sb.append(N).append("\n");
+            String line = br.readLine().trim();
+            if (line.equals("0")) break;
+            sb.append(digitalRoot(line)).append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
