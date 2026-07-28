4c523eb2bd39c49992b1ba9c382ea4b0bfc4d8f0
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2026-07-28 10:50:57 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 103.23 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -1,20 +1,15 @@
 import java.util.*;
+import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         
-        for(int i=0; i<a.length(); i++) {
-            char c = a.charAt(i);
-            if(c >= 97) {
-                c -= 32;
-            }else {
-                c += 32;
-            }
-            sb.append(c);
+        for(char c : br.readLine().toCharArray()) {
+            if(Character.isLowerCase(c)) c = Character.toUpperCase(c);
+            else c = Character.toLowerCase(c);
+            sb.append((char)(c));
         }
         System.out.println(sb.toString());
     }
