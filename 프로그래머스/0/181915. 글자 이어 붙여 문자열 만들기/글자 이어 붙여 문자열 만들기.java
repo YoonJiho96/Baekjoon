da61a17601af48e98bed49b7c1e5967a457ee27e
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2026-07-31 10:08:21 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.09 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[] index_list) {       
+        StringBuilder sb = new StringBuilder();
+        char[] str = my_string.toCharArray();
+        
+        for(int i : index_list) {
+            sb.append(str[i]);
+        }
+        return sb.toString();
+    }
+}
