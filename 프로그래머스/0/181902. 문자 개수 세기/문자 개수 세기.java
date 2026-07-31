7f9571bf6c24cc590052eeca5af88d4644b9ddf8
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2026-07-31 13:07:57 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.07 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        
+        for(char c : my_string.toCharArray()) {
+            if(c >= 97) {
+                answer[c - 'a' + 26]++;
+            }else {
+                answer[c - 'A']++;
+            }
+        }
+        
+        return answer;
+    }
+}
