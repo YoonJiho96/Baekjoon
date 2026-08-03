6a75c978aa82705b8e68ac70d5a7cc31e102c723
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2026-08-03 11:19:20 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.08 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int s, int e) {       
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string.substring(0, s))
+            .append(new StringBuilder(my_string.substring(s, e+1)).reverse())
+            .append(my_string.substring(e+1));
+        return sb.toString();
+    }
+}
