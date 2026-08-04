9c54e00c88d4adb8e050e357c1dcea3b5bb7bd8e
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2026-08-04 13:48:45 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.07 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        int size = n / k;
+        int[] answer = new int[size];
+        
+        for(int i = 0; i < size; i++) {
+            answer[i] = (i + 1) * k;
+        }
+        
+        return answer;
+    }
+}
