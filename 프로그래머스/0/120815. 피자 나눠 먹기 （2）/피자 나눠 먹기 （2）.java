f5a6168f52028121a58e55822a5c6591cb258a71
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2026-08-06 09:25:46 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int i = 1;        
+        while((i * 6) % n != 0) {
+            i++;
+        }
+        
+        return i;
+    }
+}
