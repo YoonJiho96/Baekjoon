be35240f3aa40e0a83127111d34ac74f7d8fe115
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2026-08-12 15:04:52 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        if(common[0] - common[1] == common[1] - common[2]) {
+            answer = common[common.length - 1] + (common[2] - common[1]);
+        }else {
+            answer = common[common.length -1] * (common[2] / common[1]);
+        }
+        
+        return answer;
+    }
+}
