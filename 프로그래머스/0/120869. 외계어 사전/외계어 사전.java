62bc5d16fa414290055c650baa58b523e10c3ad3
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2026-08-25 14:12:18 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -1,39 +1,14 @@
-import java.util.*;
-
 class Solution {
-    String[] tgt;
-    boolean[] selected;
-    int answer = 0;
-    HashSet<String> set = new HashSet<>();
-    
     public int solution(String[] spell, String[] dic) {
-        for(String d : dic) {
-            set.add(d);
-        }
-        
-        tgt = new String[spell.length];
-        selected = new boolean[spell.length];
-        dfs(spell, 0);
-        
-        return answer >= 1?1:2;
-    }
-    
-    void dfs(String[] spell, int dep) {
-        if(dep == tgt.length) {
-            String str = String.join("", tgt);
-            if(set.contains(str)) {
-                answer++;
-            }
-            return;
-        }
-        
-        for(int i=0; i<tgt.length; i++) {
-            if(selected[i]) continue;
+        for(String str : dic) {
+            int count = 0;
             
-            selected[i] = true;
-            tgt[dep] = spell[i];
-            dfs(spell, dep + 1);
-            selected[i] = false;
+            for(String s : spell) {
+                if(str.contains(s)) count++;
+            }
+            
+            if(count == spell.length) return 1;
         }
+        return 2;
     }
 }
