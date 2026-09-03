b9a676ca3e98fd289c002628cd9ce5087136ec7b
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2026-09-03 11:08:36 +0900

    [level 1] Title: 덧칠하기, Time: 1.37 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,18 +1,12 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int[] wall = new int[n + 1];
-        for(int s : section) {
-            wall[s] = 1;
-        }
-
         int answer = 0;
-        for(int i=1; i<=n; i++) {
-            if(wall[i] == 1) {
-                for(int j=i; j<Math.min(n, i+m); j++) {
-                    wall[j] = 1;
-                }
-                i = i + m - 1;
+        int end = 0;
+        
+        for(int s : section) {
+            if(s > end) {
                 answer++;
+                end = s + m - 1;
             }
         }
         
